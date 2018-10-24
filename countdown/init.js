7f882ef42210914a651db080480c2b0da90729b2
objectprogr <objectprogr@gmail.com>
2018-10-24 11:24:59 +0200

    Test countdown date

--- countdown/init.js
+++ countdown/init.js
@@ -1,14 +1,7 @@
-var cd = new Countdown({
-  cont: document.querySelector('.container'),
-  endDate: 1553468400000,
-  outputTranslation: {
-      year: 'Years',
-      week: 'Weeks',
-      day: 'Days',
-      hour: 'Hours',
-      minute: 'Minutes',
-      second: 'Seconds',
-  },
-  endCallback: null,
-  outputFormat: 'week|day|hour|minute|second',
+const efcc_countdown = new countdown({
+  target: '.countdown',
+  dayWord: ' days',
+  hourWord: ' hours',
+  minWord: ' mins',
+  secWord: ' secs'
 });
